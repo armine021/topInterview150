5b3d0d665068eba81f1ccb780e57ce00ac1db428
Nadine Scherer <armine021@gmail.com>
2025-08-25 20:22:33 -0700

    finish 169. Majority Element

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -59,15 +59,22 @@ class Solution {
         }
         return k;
     }
-}
+
 
 // 169. Majority Element
     public int majorityElement(int[] nums) {
-        
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
     }
-
-
-
 
 
 }
