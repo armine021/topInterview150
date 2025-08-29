e1342456cdfe3be0709fff4cf46a9a6d8d1f7ac5
Nadine Scherer <armine021@gmail.com>
2025-08-28 22:44:35 -0700

    finish 189. Rotate Array

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -78,7 +78,25 @@ class Solution {
 
 // 189. Rotate Array
     public void rotate(int[] nums, int k) {
-        
+        int n = nums.length;
+        k = k % n; // In case k > n
+
+        // Step 1: reverse entire array
+        reverse(nums, 0, n - 1);
+        // Step 2: reverse first k
+        reverse(nums, 0, k - 1);
+        // Step 3: reverse rest
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 
 
