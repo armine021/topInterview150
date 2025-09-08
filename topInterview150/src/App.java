f04c9825e3101f161cdefd21790599e37ec332a3
Nadine Scherer <armine021@gmail.com>
2025-09-07 22:03:14 -0700

    finish 122. Best Time to Buy and Sell Stock II

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -120,7 +120,13 @@ class Solution {
 
 // 122. Best Time to Buy and Sell Stock II
     public int maxProfit2(int[] prices) {
-        
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
     }
 
 }
