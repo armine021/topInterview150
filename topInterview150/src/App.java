6a871e492af775ae9996905fa59cd1be0f32a29d
Nadine Scherer <armine021@gmail.com>
2025-09-08 23:44:57 -0700

    start and finish 55. Jump Game

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -129,5 +129,23 @@ class Solution {
         return profit;
     }
 
+// 55. Jump Game
+    public boolean canJump(int[] nums) {
+        int farthest = 0; // Tracks the farthest index we can reach
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                // If the current index is beyond the farthest reachable index, return false
+                return false;
+            }
+            // Update the farthest reachable index
+            farthest = Math.max(farthest, i + nums[i]);
+            // If the farthest reachable index is beyond or at the last index, return true
+            if (farthest >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
