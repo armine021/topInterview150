b47b8a51756e54bf14650db08d9e3ec38007b430
Armine <armine021@gmail.com>
2025-09-30 20:22:48 -0700

    finish 13. Roman to Integer

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -332,8 +332,33 @@ class Solution {
     }
 
 // 13. Roman to Integer
-    public int romanToInt(String s) {
-        
+    public static int romanToInt(String s) {
+        // Map of Roman numerals
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+
+            if (curr < prev) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+
+            prev = curr;
+        }
+
+        return total;
     }
 
 }
