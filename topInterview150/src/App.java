7c09358eafe57517584ecd8f73f8bbb048684c51
Nadine Scherer <armine021@gmail.com>
2025-10-04 11:10:10 -0700

    finish 12. Integer to Roman

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -364,8 +364,20 @@ class Solution {
 // 12. Integer to Roman
 
     public String intToRoman(int num) {
+        // Define the values and corresponding Roman symbols
+        int[] values =    {1000, 900, 500, 400, 100, 90,  50, 40,  10, 9,   5,  4,   1};
+        String[] symbols = {"M",  "CM","D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
         
-    }
+        StringBuilder roman = new StringBuilder();
 
+        for (int i = 0; i < values.length && num > 0; i++) {
+            while (num >= values[i]) {
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return roman.toString();
+    }
 }
 
