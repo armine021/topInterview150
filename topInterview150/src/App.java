de6f02efd0f5757b319083bbc0115a25c825a174
Nadine Scherer <armine021@gmail.com>
2025-10-06 21:39:59 -0700

    finish 58. Length of Last Word

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -382,7 +382,21 @@ class Solution {
 
 // 58. Length of Last Word
     public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
         
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
     }
 
 }
