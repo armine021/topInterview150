6de8582d4182e2d58cd4c12886a21fce086689fb
Nadine Scherer <armine021@gmail.com>
2025-10-08 20:41:30 -0700

    finish 14. Longest Common Prefix

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -400,8 +400,20 @@ class Solution {
     }
 
 // 14. Longest Common Prefix
-    public String longestCommonPrefix(String[] strs) {
-        
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            // While the current string doesn't start with the prefix
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+
+        return prefix;
     }
 
 }
