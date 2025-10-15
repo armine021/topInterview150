c95590b8aee61afd7573e2124c4f55b2d18db558
Nadine Scherer <armine021@gmail.com>
2025-10-14 18:43:14 -0700

    start 28. Find the Index of the First Occurrence in a String

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -471,6 +471,11 @@ class Solution {
         return result.toString();
     }
 
+// 28. Find the Index of the First Occurrence in a String
+    public int strStr(String haystack, String needle) {
+        
+    }
+
 
 }
 
