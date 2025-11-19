4f447272ee1d7cdf381a04ec885bb44b72f0e236
Nadine Scherer <armine021@gmail.com>
2025-11-18 22:37:28 -0800

    finish 36. Valid Sudoku

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -851,7 +851,28 @@ class Solution {
 
     // 36. Valid Sudoku
     public boolean isValidSudoku(char[][] board) {
-        
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+
+                if (ch == '.') continue;
+
+                int num = ch - '1'; // convert '1'-'9' → 0-8
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
+                    return false; // duplicate found
+                }
+
+                rows[r][num] = cols[c][num] = boxes[boxIndex][num] = true;
+            }
+        }
+
+        return true;
     }
 
 }
