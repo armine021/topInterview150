35b5d193a8867b5d771d73fb5a95a3ef5055fb45
Nadine Scherer <armine021@gmail.com>
2025-12-20 20:53:23 -0800

    finish 202. Happy Number

--- topInterview150/src/App.java
+++ topInterview150/src/App.java
@@ -1210,7 +1210,24 @@ class Solution {
 
 // 202. Happy Number
     public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
         
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+        
+        return n == 1;
+    }
+
+    private static int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
     }
 
 }
